713240f7606f1d184fe384f632c7dcf0fc147d75
Andreas Eger <dev@eger-andreas.de>
2011-11-09 13:35:15 +0100

    commands for creating, getting and deleteing keys

--- dev_server.rb
+++ dev_server.rb
@@ -14,8 +14,8 @@ store.purpose = OpenSSL::X509::PURPOSE_SSL_CLIENT
 sslContext = OpenSSL::SSL::SSLContext.new
 sslContext.cert = OpenSSL::X509::Certificate.new(File.open("x509/server.crt"))
 sslContext.key = OpenSSL::PKey::RSA.new(File.open("x509/server.key"))
-sslContext.verify_mode = OpenSSL::SSL::VERIFY_PEER|OpenSSL::SSL::VERIFY_FAIL_IF_NO_PEER_CERT
-sslContext.cert_store = store
+#sslContext.verify_mode = OpenSSL::SSL::VERIFY_PEER|OpenSSL::SSL::VERIFY_FAIL_IF_NO_PEER_CERT
+#sslContext.cert_store = store
 
 server = TCPServer.new(listeningPort)
 ssl = OpenSSL::SSL::SSLServer.new(server, sslContext)
@@ -25,14 +25,22 @@ puts "Listening on port #{listeningPort}"
 loop do
   Thread.new(ssl.accept) do |conn|
     p "Connected to #{conn.peeraddr.last}"
-    p "Certificate verified" if conn.peer_cert.verify(OpenSSL::X509::Certificate.new(File.open("x509/cacert.pem")).public_key)
+    #p "Certificate verified" if conn.peer_cert.verify(OpenSSL::X509::Certificate.new(File.open("x509/cacert.pem")).public_key)
 
     begin
       while request = conn.gets
         $stdout.puts "=> " + request
-        response = JSON.parse(request)
-        response[:answer] = "here's your key"
-        response[:time] = Time.now
+
+        data = JSON.parse(request)
+        case data['opcode']
+        when 1
+          response = {success: true, payload: "b239rhf9b29f80whd"}
+        when 2
+          response = {success: true, payload: nil}
+        when 3
+          response = {success: true, payload: nil}
+        end
+
         $stdout.puts "<= " + response.to_json
         conn.puts response.to_json
       end
--- test_client.rb
+++ test_client.rb
@@ -1,5 +1,21 @@
 require './lib/key_client'
 
-server = KeyClient.new('127.0.0.1', 56789)
-p server.ask({question: "test foo"})
 
+host = ARGV[0]
+port = ARGV[1].to_i
+
+p "connecting to #{host}:#{port}"
+server = KeyClient.new(host,port)
+
+p 'create key'
+p server.ask({opcode: 2, keyid: "Omnonnom", key: "oldsanfo3ih342f"})
+
+p 'get a key'
+p server.ask({opcode: 1, keyid: "Omnomnom"})
+
+p 'delete key'
+p server.ask({opcode: 3, keyid: "Omnonnom"})
+
+
+# answers
+# { success: true, payload: key|error_message }
